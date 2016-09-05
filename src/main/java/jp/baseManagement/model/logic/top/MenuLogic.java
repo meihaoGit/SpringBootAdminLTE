272019c41795febf13594f4811ed5dc4543506a3
meihao <ko_bai@cm.jip.co.jp>
2016-09-05 13:42:30 +0900

    IntelliJ IDEAより、編集

--- src/main/java/jp/baseManagement/model/logic/top/MenuLogic.java
+++ src/main/java/jp/baseManagement/model/logic/top/MenuLogic.java
@@ -2,7 +2,7 @@ package jp.baseManagement.model.logic.top;
 
 /**
  * Created by 3111266 on 2016/09/01.
- * jp.dogsManagement.model.logic.top
+ * 
  */
 
 import org.springframework.stereotype.Controller;
